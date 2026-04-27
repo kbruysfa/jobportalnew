c9e80cc74fcb653b5dcf7cad6df4107b2e13eba5
kbruysfa <kbruysfa1234@gmail.com>
2026-04-27 23:23:57 +0300

    message

---
+++ jobfront/vite.config.js
@@ -0,0 +1,10 @@
+
+// https://vite.dev/config/export default defineConfig({plugins: [react()],}
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
